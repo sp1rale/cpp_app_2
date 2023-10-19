332c5d7e2b9140247be2e1abc16d3c39723a0d34
sp1rale <dimayaremchyk24@gmail.com>
2023-10-19 20:44:31 +0300

    changes

--- main.cpp
+++ main.cpp
@@ -2,5 +2,8 @@
 using namespace std;
 
 int main(){
-	cout 
+	cout << "It is impossible to get money, to be greedy, to constantly save on yourself."<<endl;
+	cout << "To be undernourished, to dress poorly, to show off oneself in everything." << endl;
+	cout << "Only those who work with pleasure can earn money  and get rich" << endl;
+	cout << "Aftor:\nHenry Ford";
 }
